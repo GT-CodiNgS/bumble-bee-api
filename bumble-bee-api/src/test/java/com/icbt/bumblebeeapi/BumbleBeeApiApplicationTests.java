2c01a3b3cb80176f9d7a9b813ea7c2757cc4e27f
GT-CodiNgS <gayasthasmika.w@gmail.com>
2023-02-02 17:40:01 +0530

    Initial commit

---
+++ bumble-bee-api/src/test/java/com/icbt/bumblebeeapi/BumbleBeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icbt.bumblebeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumbleBeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
